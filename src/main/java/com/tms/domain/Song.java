c8a9c93dd046b6114265c1d76e432bb235e93222
lizavetik <hinata_syo@mail.ru>
2023-10-03 03:23:34 +0300

    database connection

--- src/main/java/com/tms/domain/Song.java
+++ src/main/java/com/tms/domain/Song.java
@@ -1,7 +1,19 @@
 package com.tms.domain;
 
+import jakarta.persistence.*;
+
+@Entity(name = "songs")
 public class Song {
+    @Id
+    @SequenceGenerator(name = "mySeqGen", sequenceName = "songs_id_seq", allocationSize = 1)
+    @GeneratedValue(generator = "mySeqGen")
     private Integer id;
+    @Column(name = "song_name")
     private String songName;
+
+    @Column(name = "album")
     private String album;
+
+    @Column(name = "year_of_release")
+    private Integer yearOfRelease;
 }
--- src/main/java/com/tms/domain/UserInfo.java
+++ src/main/java/com/tms/domain/UserInfo.java
@@ -1,12 +1,28 @@
 package com.tms.domain;
 
+import jakarta.persistence.*;
+
 import java.time.LocalDateTime;
 
+@Entity(name = "service_user")
 public class UserInfo {
+    @Id
+    @SequenceGenerator(name="mySeqGen", sequenceName = "user_id_seq", allocationSize = 1)
+    @GeneratedValue(generator = "mySeqGen")
     private Integer id;
+
+    @Column(name = "first_name")
     private String firstName;
+
+    @Column(name = "last_name")
     private String lastName;
+
+    @Column(name = "email")
     private String email;
+
+    @Column(name = "created")
     private LocalDateTime createdAt;
-    private LocalDateTime updatedAt;
+
+    @Column(name = "updated")
+    private LocalDateTime updatedAt;;
 }
