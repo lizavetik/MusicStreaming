c6264f79763758d7caad5479af2e057f2f464d49
lizavetik <hinata_syo@mail.ru>
2023-10-07 03:25:18 +0300

    CRUD for Artist

---
+++ src/main/java/com/tms/controller/ArtistController.java
@@ -0,0 +1,48 @@
+package com.tms.controller;
+
+import com.tms.exception.NotFoundException;
+import com.tms.domain.Artist;
+import com.tms.service.ArtistService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/artist", produces = MediaType.APPLICATION_JSON_VALUE)
+public class ArtistController {
+    private final ArtistService artistService;
+
+    public ArtistController(ArtistService artistService) {
+        this.artistService = artistService;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Artist> getArtist(@PathVariable Integer id) {
+    Artist artist = artistService.getArtist(id).orElseThrow(NotFoundException::new);
+        return new ResponseEntity<>(artist, HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Artist>> getAllArtists() {
+        List<Artist> artists = artistService.getArtists();
+        if(artists.isEmpty()){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }else{
+            return new ResponseEntity<>(artists, HttpStatus.OK);
+        }
+    }
+    @PostMapping
+    public ResponseEntity<HttpStatus> createArtist(@RequestBody Artist artist) {
+        artistService.createArtist(artist);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @DeleteMapping
+    public ResponseEntity<HttpStatus> delete(@RequestBody Integer id) {
+        artistService.deleteArtistById(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+}
---
+++ src/main/java/com/tms/service/ArtistService.java
@@ -0,0 +1,33 @@
+package com.tms.service;
+
+import com.tms.domain.Artist;
+import com.tms.repository.ArtistRepository;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ArtistService {
+    private final ArtistRepository artistRepository;
+
+    public ArtistService(ArtistRepository artistRepository) {
+        this.artistRepository = artistRepository;
+    }
+    public List<Artist> getArtists() {
+            return artistRepository.findAll(Sort.by("id"));
+        }
+
+    public Optional<Artist> getArtist(Integer id) {
+            return artistRepository.findById(id);
+        }
+
+    public Artist createArtist(Artist artist) {
+        return (Artist) artistRepository.save(artist);
+    }
+    public void deleteArtistById(Integer id) {
+            artistRepository.deleteById(id);
+        }
+}
+
